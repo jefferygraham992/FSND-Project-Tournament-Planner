94d3425bff186ee0eb3e04a359d8de5a2a3ff92c
Jeffery Graham <jefferygraham496@gmail.com>
2017-01-15 20:55:29 -0500

    add files for tournament project

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
 import psycopg2
+import bleach
 
 
 def connect():
@@ -13,25 +14,51 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("DELETE FROM matches")
+    db.commit()
+    db.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("DELETE FROM players")
+    db.commit()
+    db.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("SELECT COUNT(id) AS num FROM players")
+    results = cursor.fetchone()
+    db.close()
+    if results:
+        return results[0]
+    else:
+        return '0'
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO players (name) VALUES(%s)", (bleach.clean(name),))
+    db.commit()
+    db.close()
+
+
 
 
 def playerStandings():
@@ -47,6 +74,12 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM standings")
+    results = cursor.fetchall()
+    db.close()
+    return results
 
 
 def reportMatch(winner, loser):
@@ -56,16 +89,21 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO matches (winner, loser) VALUES (%s, %s); ", (bleach.clean(winner), bleach.clean(loser)))
+    db.commit()
+    db.close()
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +111,9 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-
-
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("SELECT odds.id, odds.name, evens.id, evens.name FROM odds JOIN evens ON (odds.rnum) = (evens.rnum - 1);")
+    results = cursor.fetchall()
+    db.close()
+    return results
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -1,3 +1,6 @@
+DROP DATABASE tournament;
+CREATE DATABASE tournament;
+\c tournament;
 -- Table definitions for the tournament project.
 --
 -- Put your SQL 'create table' statements in this file; also 'create view'
@@ -5,5 +8,58 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
+
+-- players table
+CREATE TABLE players (id SERIAL PRIMARY KEY,
+                                     name TEXT);
+
+
+-- matches table
+CREATE TABLE matches (id SERIAL PRIMARY KEY,
+                                       winner INTEGER REFERENCES players (id),
+                                       loser INTEGER REFERENCES players (id));
+
+-- wins view
+CREATE VIEW num_wins AS
+            SELECT players.id, players.name, COUNT(matches.winner) as wins
+            FROM players LEFT JOIN matches
+            ON players.id = matches.winner
+            GROUP BY players.id, players.name
+            ORDER BY wins DESC;
+
+-- losses view
+CREATE VIEW num_losses AS
+            SELECT players.id, players.name, COUNT(matches.loser) as losses
+            FROM players LEFT JOIN matches
+            ON players.id = matches.loser
+            GROUP BY players.id, players.name
+            ORDER BY losses;
+
+-- view from creating the standings
+CREATE VIEW standings AS
+            SELECT num_wins.id, num_wins.name, num_wins.wins, num_wins.wins + num_losses.losses as matches
+            FROM num_losses JOIN num_wins
+            ON num_losses.id = num_wins.id
+            ORDER BY num_wins.wins DESC, num_wins.id ASC;
+
+CREATE VIEW evens AS
+            SELECT rnum, id, name
+            FROM
+            (SELECT *, row_number() over (ORDER BY standings.wins DESC) AS rnum FROM standings) AS q WHERE mod(rnum, 2) = 0;
+
+CREATE VIEW odds AS
+            SELECT rnum, id, name
+            FROM
+            (SELECT *, row_number() over (ORDER BY standings.wins DESC) AS rnum FROM standings) AS q WHERE mod(rnum, 2) != 0;
+
+CREATE VIEW pairings AS
+            SELECT odds.id, odds.name, evens.id, evens.name
+            FROM odds INNER JOIN evens
+            ON (odds.rnum) = (evens.rnum - 1);
+
+
+
+
+
 
 
